360f62e52e8245c084f0bb08008d98be70bc734b
Viktor <44034237+vittrum@users.noreply.github.com>
2019-05-18 22:05:30 +0300

    Summary?

---
+++ DB_Course/Connection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace DB_Course
+{
+    class Connection
+    {
+        private NpgsqlConnection conn;
+    }
+}
