1d73b8e0d63ddfe73f21970f0a8e3773773269a7
vittrum <viktorkovalskyi9@gmail.com>
2019-05-27 20:16:42 +0300

    Factory complete

--- DB_Course/Factory.cs
+++ DB_Course/Factory.cs
@@ -4,10 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DB_Course.Repos;
+using Npgsql;
 namespace DB_Course
 {
     class Factory : IDisposable
     {
+        public NpgsqlConnection NpgsqlConnection;
+        public SqlConnection SqlConnection;
         public RepositoryBusiness_Trip RepositoryBusiness_Trip
         {
             get => RepositoryBusiness_Trip;
@@ -83,8 +86,55 @@ namespace DB_Course
             get => RepositoryVacations;
             set => RepositoryVacations = value;
         }
+        private bool Disposed = false;
 
+        public Factory(string server, string port, string user, string pass, string dbname)
+        {
+            string ConnectionString = "Server=" + server + "; Port=" + port + "; User Id=" + user + "; Password=" + pass + "; Database=" + dbname + ";";
+            NpgsqlConnection = new NpgsqlConnection(ConnectionString);
+            SqlConnection = new SqlConnection(NpgsqlConnection);
+            OpenConnection();
+            RepositoryBusiness_Trip = new RepositoryBusiness_Trip(SqlConnection);
+            RepositoryChair = new RepositoryChair(SqlConnection);
+            RepositoryContract = new RepositoryContract(SqlConnection);
+            RepositoryDegree = new RepositoryDegree(SqlConnection);
+            RepositoryEmployee_Sheet = new RepositoryEmployee_Sheet(SqlConnection);
+            RepositoryOrder = new RepositoryOrder(SqlConnection);
+            RepositoryPositions = new RepositoryPositions(SqlConnection);
+            RepositorySick_List = new RepositorySick_List(SqlConnection);
+            RepositoryStaff = new RepositoryStaff(SqlConnection);
+            RepositoryStaff_Degree = new RepositoryStaff_Degree(SqlConnection);
+            RepositoryStaff_Title = new RepositoryStaff_Title(SqlConnection);
+            RepositoryTime_Sheet = new RepositoryTime_Sheet(SqlConnection);
+            RepositoryTitle = new RepositoryTitle(SqlConnection);
+            RepositoryTransfer_Training = new RepositoryTransfer_Training(SqlConnection);
+            RepositoryVacations = new RepositoryVacations(SqlConnection);
+        }
 
+        public void OpenConnection()
+        {
+            SqlConnection.Connection.Open();
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        public void Dispose(bool disposing)
+        {
+            if (!Disposed)
+            {
+                if (disposing)
+                {
+                    SqlConnection.Connection.Close();
+                }
+                Disposed = true;
+            }
+        }
+        ~Factory()
+        {
+            Dispose(false);
+        }
 
     }
 }
